89952bea568c98dcea2a1f16382d53c8ce41e0ae
melanie-demeure <71132980+melanie-demeure@users.noreply.github.com>
2020-11-24 10:14:30 +0100

    nouveau jdd

--- Visu_Loïc.r
+++ Visu_Loïc.r
@@ -5,12 +5,12 @@ library(tidyverse)
 
 
 
-#Importation jeux de données (DOM TOM et autres enlevé à la main sur excel)
-pubpriv<-read.csv("C:/Users/home/OneDrive - etu.unistra.fr (1)/M2 Data Sciences/Visualisation Ordi OK/pubpriv.csv", sep=";",dec=".", header=TRUE)
+#Importation jeux de donn?es (DOM TOM et autres enlev? ? la main sur excel)
+pubpriv<-read.csv("pubpriv.csv", sep=";",dec=".", header=TRUE)
 pubpriv<-as.data.frame(pubpriv)
 
 #Conversion des variables en facteurs
-pubpriv$ï..code_zone_geo <- as.factor(pubpriv$ï..code_zone_geo)
+pubpriv$Ã¯..code_zone_geo <- as.factor(pubpriv$Ã¯..code_zone_geo)
 pubpriv$zone_geographique <- as.factor(pubpriv$zone_geographique)
 pubpriv$code_type_org <- as.factor(pubpriv$code_type_org)
 pubpriv$type_d_organisme <- as.factor(pubpriv$type_d_organisme)
@@ -23,29 +23,31 @@ pubpriv$non_disponible <- as.factor(pubpriv$non_disponible)
 pubpriv$etat_des_donnees <- as.factor(pubpriv$etat_des_donnees)
 
 
-#Filtrer en fonction de l'année, du type d'organisme, et du code indicateur
+#Filtrer en fonction de l'ann?e, du type d'organisme, et du code indicateur
 pubpriv %>%
   filter(annee==2013)%>%
   filter(type_d_organisme=="Entreprises")%>%
   filter(code_indicateur=="dird")%>%
-  arrange(ï..code_zone_geo)
+  arrange(Ã¯..code_zone_geo)
   
 
-#Fond de carte France par région 2014
-reg<-st_read(dsn="C:/Users/home/OneDrive - etu.unistra.fr (1)/M2 Data Sciences/Visualisation Ordi OK/regions-20140306-100m-shp/regions-20140306-100m.shp")
+#Fond de carte France par r?gion 2014
+reg<-st_read(dsn="regions-20140306-100m-shp/regions-20140306-100m.shp")
 reg$nom
-#On enlève les DOM TOM
+#On enl?ve les DOM TOM
 reg<-reg[-(11:12),]
 reg<-reg[-(14),]
 reg<-reg[-(16:17),]
 
-#Vérification du fond de carte
+#V?rification du fond de carte
 reg %>%
   ggplot() + geom_sf()
 
-#On merge les données et le fond de carte
-test<-merge(x=reg,y=pubpriv,by.x="code_insee",by.y="ï..code_zone_geo",all.x=T)
+#On merge les donn?es et le fond de carte
+test<-merge(x=reg,y=pubpriv,by.x="code_insee",by.y="Ã¯..code_zone_geo",all.x=T)
 
+summary(pubpriv)
+test
 
 #Test Graphique 1
 test %>% ggplot2::ggplot() +
@@ -58,3 +60,20 @@ test %>%
   geom_sf(aes(fill = as.numeric(valeur))) +
   scale_fill_gradient(low = "#75c9c5", high = "#fb1c05") +
   labs(fill = 'valeur')
+
+
+# nombre de chercheurs par rÃ©gions
+person <- pubpriv %>%
+  filter(annee==2013)%>%
+  filter(type_d_organisme=="Total administrations et entreprises")%>%
+  filter(code_indicateur=="pers")%>%
+  filter(type_de_personnel=="Tous types de personnel")%>%
+  arrange(Ã¯..code_zone_geo)
+
+#On merge les donnees et le fond de carte
+test<-merge(x=reg,y=person,by.x="code_insee",by.y="Ã¯..code_zone_geo",all.x=T)
+
+test %>% ggplot2::ggplot() +
+  geom_sf( aes(fill = valeur) )
+
+pubpriv(which(is.na(pubpriv$valeur))
