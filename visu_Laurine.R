942c22f9ed1d5e0fada2ee6b68521b0ebd434706
Laurine-Allard <71133649+Laurine-Allard@users.noreply.github.com>
2020-11-26 15:29:40 +0100

    Update visu_Laurine.R

--- visu_Laurine.R
+++ visu_Laurine.R
@@ -31,44 +31,121 @@ mayotte<-reg[20,]
 
 reg<-reg[-c(11:12,16,19:20),]
 
+reg$nom
+
 # Visualisation du fond de carte avec changement du CRS avec UTM31N
 reg %>%
   st_transform( crs = 32631 )%>% 
   ggplot() + geom_sf()+
   theme_light()
 
-#Filtrer en fonction de l'annee, du type d'organisme, et du code indicateur
-puborga %>%
-  filter(annee==2013)%>%
-  filter(type_d_organisme=="Entreprises")%>%
-  filter(code_indicateur=="dird")%>%
-  arrange(code_region)
-
 #On merge les donnees et le fond de carte
 test<-merge(x=reg,y=puborga,by.x="code_insee",by.y="code_region",all.x=T)
 
-# graph avec evolution des chercherus h/F dans le temps au niveau national. + 2 dates éloignées, au sein des régions comme ça évolue en termes de proportion (camembert par région)
+##################################
+# Graphique de l'evolution de l'effectif des types d'administration au niveau national de 2001 à 2013
+df1 <- puborga %>%
+  filter(indicateur=="Effectifs de R&D")%>%
+  filter(code_indicateur=="pers")%>%
+  #arrange(type_d_administration) %>%
+  group_by(type_d_administration,annee) %>%
+  summarise(total_type_admin=sum(valeur))
+df1
+
+df1 %>% 
+  ggplot(aes(x = annee, y = total_type_admin,group=type_d_administration,color=type_d_administration))+
+  geom_line() + theme_light() + xlab("Année") + ylab("Effectif") + ggtitle ("Evolution de l'effectif des types d'administration au niveau national") +
+  theme(axis.text.x = element_text(angle=45, vjust = 0.5)) +
+  labs(color="Types d'administration") +
+  theme(legend.position="bottom") +
+  theme(legend.title.align=0.5) +
+  guides(colour = guide_colourbar(title.vjust = 0.9)) +
+  guides(colour = guide_legend(title.position = "top", nrow = 2)) +
+  theme(legend.title = element_text(face="bold")) +
+  theme(plot.title = element_text(hjust = 1, vjust = 1.8, face = "bold"))
 
 
+##################################
+# Graphique de l'evolution de des depenses des types d'administration au niveau national de 2001 à 2013
+df2 <- puborga %>%
+  filter(indicateur=="Depense interieure de R&D")%>%
+  filter(code_indicateur=="dird")%>%
+  #arrange(type_d_administration) %>%
+  group_by(type_d_administration,annee) %>%
+  summarise(total_type_admin=sum(valeur))
+df2
+
+df2 %>% 
+  ggplot(aes(x = annee, y = total_type_admin,group=type_d_administration,color=type_d_administration))+
+  geom_line() + theme_light() + xlab("Année") + ylab("Dépenses (millions d'€)") + ggtitle ("Evolution des dépenses des types d'administration au niveau national") +
+  theme(axis.text.x = element_text(angle=45, vjust = 0.5)) +
+  labs(color="Types d'administration") +
+  theme(legend.position="bottom") +
+  theme(legend.title.align=0.5) +
+  guides(colour = guide_colourbar(title.vjust = 0.9)) +
+  guides(colour = guide_legend(title.position = "top", nrow = 2)) +
+  theme(legend.title = element_text(face="bold")) +
+  theme(plot.title = element_text(size = 12, hjust = 1, vjust = 1.8, face = "bold"))
 
 
+#2001 ----------------------------------------------------
 
-# Differents types d'administration
+#Préparation du jeu de données pour 2001
+dep_tot<-puborga %>%
+  filter(annee==2001) %>%
+  filter(indicateur=="Depense interieure de R&D")%>%
+  group_by(code_region,region)%>%
+  summarise(Depense_Total=sum(valeur))
+dep_tot<-as.data.frame(dep_tot)
 
-# on filtre
-# puis on merge sur ce quon a filtré
+eff_tot<-puborga %>%
+  filter(annee==2001) %>%
+  filter(indicateur=="Effectifs de R&D")%>%
+  group_by(code_region,region)%>%
+  summarise(Effectif_Total=sum(valeur))
+eff_tot<-as.data.frame(eff_tot)
 
 
-#Test Graphique 1
-test %>% ggplot2::ggplot() +
-  geom_sf( aes(fill = valeur) )
-test
+dep_eff_tot<-cbind(dep_tot,eff_tot)
+dep_eff_tot<-dep_eff_tot[-1,]
+dep_eff_tot<-dep_eff_tot[-23,]
+dep_eff_tot<-dep_eff_tot[,-(4:5)]
 
-#Test Graphique 2
-test %>%
-  ggplot() +
-  geom_sf(aes(fill = as.numeric(valeur))) +
-  scale_fill_gradient(low = "#75c9c5", high = "#fb1c05") +
-  labs(fill = 'valeur')
-test
+#IMPORTANT jeu de données final 2001
+filter_dep_eff_tot_2001<-
+  dep_eff_tot%>%
+  mutate(Depense_Effectif=Depense_Total/Effectif_Total)
+
+
+#2013 ----------------------------------------------------
+
+#Préparation du jeu de données pour 2013
+dep_tot_2013<-
+  rdFR %>%
+  filter(annee==2013) %>%
+  filter(indicateur=="Depense interieure de R&D")%>%
+  group_by(code_region,region)%>%
+  summarise(Depense_Total=sum(valeur))
+dep_tot_2013<-as.data.frame(dep_tot_2013)
+dep_tot_2013<-dep_tot_2013[-(1:6),]
+dep_tot_2013<-dep_tot_2013[-23,]
+
+eff_tot_2013<-
+  rdFR %>%
+  filter(annee==2001) %>%
+  filter(indicateur=="Effectifs de R&D")%>%
+  group_by(code_region,region)%>%
+  summarise(Effectif_Total=sum(valeur))
+eff_tot_2013<-as.data.frame(eff_tot_2013)
+eff_tot_2013<-eff_tot_2013[-1,]
+eff_tot_2013<-eff_tot_2013[-23,]
+
+dep_eff_tot_2013<-cbind(dep_tot_2013,eff_tot_2013)
+dep_eff_tot_2013<-dep_eff_tot_2013[,-(4:5)]
+
+
+#IMPORTANT jeu de données final 2013
+filter_dep_eff_tot_2013<-
+  dep_eff_tot_2013%>%
+  mutate(Depense_Effectif=Depense_Total/Effectif_Total)
 
